c9f711d0d8ec0103e423ab6808403721110db850
Dcd-hhs <dcdoedens@hhs.nl>
2022-04-10 18:10:17 +0200

    New start for a python interface.

---
+++ ardjoy.py
@@ -0,0 +1,24 @@
+#!/usr/python3
+"""
+Author: D.C.Doedens
+Date: 20220410
+Goal:
+    Use input from joystick to generate and plot data in matplotlib
+    Both this programme and the programme on the joystick use the ibus protocol
+    based on/ inspired by VJoySerialFeeder
+
+TODO:
+    - Get the serial stream of data
+      - Connect to serial
+      - Receive data
+        - decode data
+    - Plot the joydata in a 'live' plot with matplotlib
+      - get matplotlib to work with live data
+        - stuff..
+    - Genereate live data through another source, and modify the plot settings
+      using the joydata
+    - change to gitlab?
+
+DONE:
+    - setup the Github thing
+"""
